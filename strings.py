81f58aa2cd171241fed0b83b5bd1075614d1fbd4
mr-shan <55774764+mr-shan@users.noreply.github.com>
2022-11-23 10:59:59 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,28 @@
+exmp = "This is string's example"
+exmp2 = 'This is string"s example'
+
+mut_str = """This is another string"""
+mul_str = """This is multi line string\\
+\\
+Here is the proof\\
+1. poof\\
+2. doom\\
+
+"""
+
+# single use of \ in tripple quote string makes it single line
+# use escape character \ two enter \ in stripple quote string
+
+raw_str = r"This is raw string \ No escape chars needed here \\"
+
+print(exmp)
+print(exmp2)
+print(mut_str)
+print(mul_str)
+print(raw_str)
+
+print("")
+
+print(5/2)
+
+print("End of the program")
